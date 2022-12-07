2e4a012a2df7a7ffe52d7677f9f5b31c0f61fa78
Matt Millican <matt@mattmillican.com>
2022-12-06 23:34:18 -0600

    day 06

---
+++ python/day06/part1.py
@@ -0,0 +1,27 @@
+import os.path
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def compute(input: str) -> int:
+    mark_seq = []
+
+    for c_idx, c in enumerate(input):
+        mark_seq.append(c)
+
+        if c_idx >= 4 and len(set(mark_seq)) == 4:
+            return c_idx + 1
+        elif len(mark_seq) >= 4:
+            mark_seq.pop(0)
+
+        print(f'seq: {mark_seq}')
+    
+    return 0
+
+def main() -> int:
+    with open(INPUT_PATH) as f:
+        print(compute(f.read()))
+    
+    return 0
+
+if __name__ == '__main__':
+    raise SystemExit(main())
